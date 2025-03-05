e2de0539da97e1e53c631e1e9bc540fee1c22923
qingshanhedragonplus <qingshan.he@dragonplus.com>
2025-03-05 17:34:34 +0800

    更新 "图片导入自动成为sprite"

---
+++ Test/Assets/Editor/TexturePostprocessor.cs
@@ -0,0 +1,20 @@
+using UnityEditor;
+using UnityEngine;
+
+public class TexturePostprocessor : AssetPostprocessor
+{
+    void OnPreprocessTexture()
+    {
+        var importer = (TextureImporter)assetImporter;
+        
+        // 通用设置：所有图片设为Sprite模式
+        importer.textureType = TextureImporterType.Sprite;
+
+        // 特殊处理Spine资源
+        if (importer.assetPath.Contains("Spine/")) // 根据实际路径调整
+        {
+            importer.alphaIsTransparency = false;
+            Debug.Log($"Processed Spine texture: {importer.assetPath}");
+        }
+    }
+}
